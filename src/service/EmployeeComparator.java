a90faca03255ac743cee75c175342bef8f3109ab
unknown <jahanshir.sm92@gmail.com>
2021-10-24 07:04:27 -0700

    add class EmployeeComparator and implement Comparator

--- src/service/EmployeeComparator.java
+++ src/service/EmployeeComparator.java
@@ -9,6 +9,11 @@ public class EmployeeComparator implements Comparator<Employee> {
 
     @Override
     public int compare(Employee o1, Employee o2) {
-return  0;
+        if (o1.compareTo(o2)==0) {
+            return Long.compare(o1.getSalary(), o2.getSalary());
+        }
+        else
+            return 1;
+
     }
 }
