ef995bbd476826bd72c264cfe3ff39bfc17663b4
unknown <jahanshir.sm92@gmail.com>
2021-10-24 05:01:35 -0700

    add class EmployeeComparator

---
+++ src/service/EmployeeComparator.java
@@ -0,0 +1,14 @@
+package service;
+
+import model.Employee;
+
+import java.util.Comparator;
+
+public class EmployeeComparator implements Comparator<Employee> {
+
+
+    @Override
+    public int compare(Employee o1, Employee o2) {
+return  0;
+    }
+}
